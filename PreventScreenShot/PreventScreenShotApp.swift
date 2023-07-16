6ac3de94b2d1c566bba2040df26bcf73921b13b7
bilal simsek <xyzbilal@gmail.com>
2023-07-16 14:59:24 +0300

    Initial Commit

---
+++ PreventScreenShot/PreventScreenShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreventScreenShotApp.swift
+//  PreventScreenShot
+//
+//  Created by Bilal SIMSEK on 16.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct PreventScreenShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
